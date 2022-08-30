c2517b065f72e609aa74edccfd9f7d835b379514
Pablo Asensio Martínez <44872361+PabloAsensio@users.noreply.github.com>
2022-08-30 20:17:16 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -15,6 +15,9 @@ ln -sf configs/.bash_aliases .bash_aliases
 ln -sf configs/.vimrc .vimrc
 ln -sf configs/.zshrc .zshrc
 ln -sf configs/powerlevel10k powerlevel10k
+
+# ZSH
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 ln -sf configs/.oh-my-zsh .oh-my-zsh
 source ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+chsh -s $(which zsh)
