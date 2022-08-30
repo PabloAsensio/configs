27eafc85b3e304520a3bede26dfb1678af3b66b7
Pablo Asensio Martínez <44872361+PabloAsensio@users.noreply.github.com>
2022-08-30 20:18:38 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -20,4 +20,4 @@ ln -sf configs/powerlevel10k powerlevel10k
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 ln -sf configs/.oh-my-zsh .oh-my-zsh
 source ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-chsh -s $(which zsh)
+sudo chsh -s $(which zsh)
